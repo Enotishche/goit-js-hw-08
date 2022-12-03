b0b901828b43455ff5266db231543af081dec1ef
Alona-Panchenko <97801671+Enotishche@users.noreply.github.com>
2022-12-03 15:55:29 +0200

    ДЗ 8
    данные выводятся в консоль в виде обьекта, а не строк

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,32 +2,59 @@ import throttle from "lodash.throttle";
 
 const STORAGE_KEY = "feedback-form-state";
 const formEl = document.querySelector(".feedback-form");
+const email = document.querySelector(" .feedback-form input");
+const message = document.querySelector(".feedback-form textarea");
+
+const persistedFormData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
 populateInput();
 formEl.addEventListener("input", throttle(onInputChange, 500));
 formEl.addEventListener("submit", onSubmitForm);
 
 function onSubmitForm(event) {
+    // event.preventDefault();
+    // const formData = new FormData(formEl);
+    // formData.forEach((value, name) => console.log(value, name))
+    // event.currentTarget.reset();
+    // localStorage.removeItem(STORAGE_KEY);
+
     event.preventDefault();
-    const formData = new FormData(formEl);
-    formData.forEach((value, name) => console.log(value, name))
+    persistedFormData.email = email.value;
+    persistedFormData.message = message.value;
+    console.log(persistedFormData);
     event.currentTarget.reset();
     localStorage.removeItem(STORAGE_KEY);
+    for (const prop of Object.keys(persistedFormData)) {
+        delete persistedFormData[prop];
+    }
+
 }
 
 function onInputChange(event) {
-    let persistedFormData = localStorage.getItem(STORAGE_KEY);
-    persistedFormData = persistedFormData ? JSON.parse(persistedFormData) : {};
+    // let persistedFormData = localStorage.getItem(STORAGE_KEY);
+    // persistedFormData = persistedFormData ? JSON.parse(persistedFormData) : {};
+    // persistedFormData[event.target.name] = event.target.value;
+    // localStorage.setItem(STORAGE_KEY, JSON.stringify(persistedFormData));
+
+    
     persistedFormData[event.target.name] = event.target.value;
     localStorage.setItem(STORAGE_KEY, JSON.stringify(persistedFormData));
+    
 }
 
 function populateInput() {
-    let persistedFormData = localStorage.getItem(STORAGE_KEY);
-    if (persistedFormData) {
-        persistedFormData = JSON.parse(persistedFormData);
-        Object.entries(persistedFormData).forEach(([name, value]) => {
-            formEl.elements[name].value = value;
-        })
+    // let persistedFormData = localStorage.getItem(STORAGE_KEY);
+    // if (persistedFormData) {
+    //     persistedFormData = JSON.parse(persistedFormData);
+    //     Object.entries(persistedFormData).forEach(([name, value]) => {
+    //         formEl.elements[name].value = value;
+    //     })
+    // }
+
+    const storageData = localStorage.getItem(STORAGE_KEY);
+    const finalData = JSON.parse(storageData);
+    if (finalData) {
+        email.value = finalData.email || " ";
+        message.value = finalData.message || " ";
     }
 }
 
